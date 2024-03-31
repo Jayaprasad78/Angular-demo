b6a7538c6cc86b9f59646f757d896240f75a147d
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-03-31 14:07:03 +0530

    Update signin.component.ts

--- src/app/container/signin/signin.component.ts
+++ src/app/container/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -7,21 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SigninComponent {
 
-  constructor(private http: HttpClient) {}
 
-  sendRequest() {
-    this.http.get<any>('https://your-backend-url/jp').subscribe(
-      (response) => {
-        console.log(response); // Log the response for debugging
-        if (response === 'ok') {
-          alert('Successful'); // Display alert if response is 'ok'
-        }
-      },
-      (error) => {
-        console.error('Error:', error); // Log error if request fails
-      }
-    );
-  }
 }
 
 
