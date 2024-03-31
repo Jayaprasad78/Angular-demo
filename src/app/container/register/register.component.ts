ccdc75c4cedb299a08a39d5ff16cd79583752e18
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-03-31 08:26:28 +0530

    Update register.component.ts

--- src/app/container/register/register.component.ts
+++ src/app/container/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -18,48 +17,47 @@ export class RegisterComponent {
   constructor(private http: HttpClient) {}
 
   onSubmit() {
-    
     let bodyData = {
       name: this.name,
       email: this.email,
       phone: this.phone,
       work: this.work,
       password: this.password,
-      // Assuming you have a backend that expects a 'cpassword' field
       cpassword: this.cpassword 
     };
 
     this.http.post('https://angularback1.vercel.app/signup', bodyData, { responseType: 'text' })
-    .subscribe(
-      (resultData: any) => {
-        console.log(resultData);
-        alert('Registered Successfully');
-  
-        // Resetting the form after successful registration
-        this.name = '';
-        this.email = '';
-        this.phone = 0;
-        this.work = '';
-        this.password = '';
-        this.cpassword = '';
-      },
-      (error) => {
-        console.error('Error during registration:', error);
-
-        if (error.status === 400) {
-          alert('Validation failed. Please fill in all the fields.');
-        } else if (error.status === 401) {
-          alert('Email is already registered.');
-        } else if (error.status === 402) {
-          alert('Passwords do not match.');
-        } else {
-          alert('An error occurred during registration.');
+      .subscribe(
+        (resultData: any) => {
+          console.log(resultData);
+          alert('Registered Successfully');
+          this.resetForm();
+        },
+        (error) => {
+          console.error('Error during registration:', error);
+          this.handleRegistrationError(error);
         }
-       
-      }
-      
-    );
-    }  
-}
+      );
+  }  
 
-  
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.phone = 0;
+    this.work = '';
+    this.password = '';
+    this.cpassword = '';
+  }
+
+  handleRegistrationError(error: any) {
+    if (error.status === 400) {
+      alert('Validation failed. Please fill in all the fields.');
+    } else if (error.status === 401) {
+      alert('Email is already registered.');
+    } else if (error.status === 402) {
+      alert('Passwords do not match.');
+    } else {
+      alert('An error occurred during registration.');
+    }
+  }
+}
